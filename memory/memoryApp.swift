8e1c5481d61ef74d26bddf3993d2ac8543d61685
Manbir Rakhra <manbir.s.rakhra@gmail.com>
2023-03-25 22:30:00 -0600

    Initial Commit

---
+++ memory/memoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  memoryApp.swift
+//  memory
+//
+//  Created by manbir rakhra on 25/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct memoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
